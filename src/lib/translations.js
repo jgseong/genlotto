00242104a5a705a174d4e080a4669c4758fc2768
jgseong <jgseong7@naver.com>
2023-08-09 01:27:40 +0900

    integrate the tailwindcss and i18n

---
+++ src/lib/translations.js
@@ -0,0 +1,12 @@
+export default {
+  en: {
+    "home.title": "Lotto Number Generator",
+    "home.outtitle": "Extracted Numbers",
+    "home.button": "Extract Numbers",
+  },
+  ko: {
+    "home.title": "로또 번호 배열 생성",
+    "home.outtitle": "추출된 번호 배열",
+    "home.button": "번호 추출",
+  },
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,13 @@
 <script>
-	let pivotNumbers = [];
+  //import Tailwindcss from "$lib/tailwindcss.svelte";
+  import { t, locale, locales } from '$lib/i18n.js';
+
+  let pivotNumbers = [];
   let extractedNumbers = [];
 
   function extractNumbers() {
+    pivotNumbers = [];
+    extractedNumbers = [];
     let numbers = Array.from({ length: 45 }, (_, i) => i + 1);
     for (let i = 0; i < 5; i++) {
       let randomIndex = Math.floor(Math.random() * numbers.length);
@@ -20,15 +25,29 @@
 			extractedNumbers = [...extractedNumbers, tmpNumbers];
 		}
   }
+
+  $locale = 'ko';
 </script>
 
 <main>
-  <h1>Random Number Extractor</h1>
-  <button on:click={extractNumbers}>Extract Numbers</button>
-
+  <p>
+    <select bind:value={$locale}>
+      {#each locales as l}
+        <option value={l}>{l}</option>
+      {/each}
+    </select>
+  </p>
+  <h1 class="text-center text-base font-semibold leading-7 text-gray-900"
+  >{$t('home.title')}</h1>
+  <div class="text-center">
+    <button class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+      on:click={extractNumbers}
+    >{$t('home.button')}</button>
+  </div>
   {#if extractedNumbers.length > 0}
-    <h2>Extracted Numbers</h2>
-    <ul>
+    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+    >{$t('home.outtitle')}</h2>
+    <ul class='text-center'>
       {#each extractedNumbers as number}
         <li>{number.join(' ')}</li>
       {/each}
