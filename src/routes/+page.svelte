26d861a6d7c9b4ec87006dd3f59063e4efc6fe6c
jgseong <jgseong7@naver.com>
2023-08-09 00:12:23 +0900

    implement a lotto generator

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let pivotNumbers = [];
+  let extractedNumbers = [];
+
+  function extractNumbers() {
+    let numbers = Array.from({ length: 45 }, (_, i) => i + 1);
+    for (let i = 0; i < 5; i++) {
+      let randomIndex = Math.floor(Math.random() * numbers.length);
+      pivotNumbers = [...pivotNumbers, numbers[randomIndex]];
+      numbers.splice(randomIndex, 1);
+    }
+		for (let i = 1; i <= 45; i++) {
+			let tmpNumbers = [...pivotNumbers];
+			if (tmpNumbers.includes(i)) {
+				continue;
+			}
+			tmpNumbers = [...tmpNumbers, i];
+			tmpNumbers = tmpNumbers.sort((a, b) => a - b);
+			console.log(tmpNumbers);
+			extractedNumbers = [...extractedNumbers, tmpNumbers];
+		}
+  }
+</script>
+
+<main>
+  <h1>Random Number Extractor</h1>
+  <button on:click={extractNumbers}>Extract Numbers</button>
+
+  {#if extractedNumbers.length > 0}
+    <h2>Extracted Numbers</h2>
+    <ul>
+      {#each extractedNumbers as number}
+        <li>{number.join(' ')}</li>
+      {/each}
+    </ul>
+  {/if}
+</main>
+
+<style>
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  li {
+    margin: 0.5rem;
+  }
+</style>
